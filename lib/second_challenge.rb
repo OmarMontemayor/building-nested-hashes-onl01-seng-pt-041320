30e22f906f6305bac46b9dfbfe15e13474a7b6ff
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-25 01:59:12 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,16 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
-      }
-  }
-  
+   }
+}
 end
