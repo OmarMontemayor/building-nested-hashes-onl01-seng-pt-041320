0332ddb4ff5ae768248149c32365f8861e663d9b
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-25 01:58:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,14 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {}
-      :matriarch ={}
+      :matriarch => {}
       :hero => {}
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {}
-      :matriarch ={}
+      :matriarch => {}
       :heroine => {}
       :heroine_friends => []
       }
